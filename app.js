e2d7fd05060df7749d0b54114f8e8f118d7ed3ee
nick777977 <nick.push777977@gmail.com>
2021-10-10 14:48:40 +0300

    add routes handling

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => { //  .use() method will be triggered on every req
-  console.log('In the middleware!');
+app.use('/', (req, res, next) => { //  .use() method will be triggered on every req
+  console.log('this always runs');
   next(); // Allows the request to continue to the next middleware in line
 });
 
-app.use((req, res, next) => {
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>Add product page!</h1>'); // .send() auto-sets header for content type if u r passing a string
+});
+
+app.use('/', (req, res, next) => { // the string param of url means it should start on these letters
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>'); // .send() auto-sets header for content type if u r passing a string
 });
