c659c6c24dcaf1b32f8cc25e39e102acd210a917
nick777977 <nick.push777977@gmail.com>
2021-10-11 17:22:06 +0300

    complete pug setup for all views

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.set('view engine', 'pug'); // use pug template engine
-app.set ('views', 'views'); // find templates in views folder
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -17,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
